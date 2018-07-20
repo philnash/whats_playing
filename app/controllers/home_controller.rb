ef7b78c2c51850801b9cff99b489974bedc71b84
Phil Nash <philnash@gmail.com>
2018-07-20 18:13:18 +1000

    Fix home controller tests

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -6,5 +6,6 @@ class HomeController < ApplicationController
   def callback
     @spotify_user = RSpotify::User.new(request.env['omniauth.auth'])
     @new_playlist = @spotify_user.create_playlist!('WhatsPlaying')
+    render :callback
   end
 end
--- test/controllers/home_controller_test.rb
+++ test/controllers/home_controller_test.rb
@@ -9,7 +9,13 @@ class HomeControllerTest < ActionDispatch::IntegrationTest
   test "should get callback" do
     Rails.application.env_config["omniauth.auth"] = OmniAuth.config.mock_auth[:twitter]
     mock_spotify_user = Minitest::Mock.new
-    mock_spotify_user.expect :create_playlist!, nil, ['WhatsPlaying']
+    mock_playlist = Minitest::Mock.new
+    mock_playlist.expect :id, 'id'
+    mock_spotify_user.expect :create_playlist!, mock_playlist, ['WhatsPlaying']
+    mock_spotify_user.expect :id, 'id'
+    credentials = { 'token' => 'spotify-token', 'refresh_token' => 'spotify-refresh-token' }
+    mock_spotify_user.expect :credentials, credentials
+    mock_spotify_user.expect :credentials, credentials
     RSpotify::User.stub :new, mock_spotify_user do
       get auth_spotify_callback_url
       assert_response :success
