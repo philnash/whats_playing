2b1aaacee80367af25b1234255af52ad3c77ad29
Phil Nash <philnash@gmail.com>
2018-07-20 14:37:31 +1000

    Warn in RSpotify initializer if credentials are required.

--- config/initializers/rspotify.rb
+++ config/initializers/rspotify.rb
@@ -1,7 +1,14 @@
-RSpotify.authenticate(ENV["SPOTIFY_CLIENT_ID"], ENV["SPOTIFY_CLIENT_SECRET"])
+begin
+  require 'rspotify/oauth'
 
-require 'rspotify/oauth'
+  RSpotify.authenticate(ENV["SPOTIFY_CLIENT_ID"], ENV["SPOTIFY_CLIENT_SECRET"])
 
-Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :spotify, ENV["SPOTIFY_CLIENT_ID"], ENV["SPOTIFY_CLIENT_SECRET"], scope: 'playlist-modify-public'
+  Rails.application.config.middleware.use OmniAuth::Builder do
+    provider :spotify, ENV["SPOTIFY_CLIENT_ID"], ENV["SPOTIFY_CLIENT_SECRET"], scope: 'playlist-modify-public'
+  end
+rescue RestClient::BadRequest => e
+  puts "=======\n" \
+       "Missing Spotify client credentials. Please enter SPOTIFY_CLIENT_ID\n" \
+       "and SPOTIFY_CLIENT_SECRET in config/env.yml.\n" \
+       "======="
 end
