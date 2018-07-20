683bfbf640dbfdcb536c02255ff3234f84fb2ad4
Phil Nash <philnash@gmail.com>
2018-07-20 12:27:53 +1000

    Don't generate JS and CSS for controllers

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -43,6 +43,10 @@ Rails.application.configure do
   # Suppress logger output for asset requests.
   config.assets.quiet = true
 
+  # Stop generating assets
+  config.generators.stylesheets = false
+  config.generators.javascripts = false
+
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
 
