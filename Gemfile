19669030444f388f53c8dcf1ddcfd0bce9a7466d
Phil Nash <philnash@gmail.com>
2018-08-27 12:27:59 +1000

    Protect endpoint with Twilio webhook authentication

--- Gemfile
+++ Gemfile
@@ -26,7 +26,7 @@ gem 'twilio-ruby', '~> 5.12.3'
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
-  gem 'envyable'
+  gem 'envyable', require: 'envyable/rails-now'
 end
 
 group :development do
--- config/application.rb
+++ config/application.rb
@@ -26,5 +26,7 @@ module Whatsplaying2
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+
+    config.middleware.use Rack::TwilioWebhookAuthentication, ENV['TWILIO_AUTH_TOKEN'], /\/twilio\/messages/
   end
 end
